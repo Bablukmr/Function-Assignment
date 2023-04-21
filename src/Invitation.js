20bafbd6790ec35f6af812efe2db71d4225407d8
Bablukmr <bablukmr800@gmail.com>
2023-04-21 15:17:47 +0530

    Day3-solution

--- src/Invitation.js
+++ src/Invitation.js
@@ -8,13 +8,14 @@ function Invitation(props){
     partyname: " birthday party",
     otherguest:"Ritu , Saurabh , Kartik",
     near: "Green field Avenue",
-    sirname:<h5>JaGdish</h5>
+    sirname:"JaGdish"
 }
    //partyname,otherguest,near,from}
     return(
         <Fragment>
     <h5>Subject:{details.Subject}</h5>
     <h5>To:{details.To}</h5>
+    <h5>Hi {details.sirname} </h5>
 <div> I am having a {details.partyname} next Friday at my Home . Would you like to come ? It will be fun . Lots of people from my school are coming . you know some of them -{details.otherguest}.
 <br></br>
 <br></br>
@@ -23,7 +24,8 @@ ehind our school , near {details.near} .
 <br></br>
 I hope you will come and see you soon .
 <br></br>
-from,{details.sirname}
+from,<br></br>
+{details.sirname}
 
 </div>
         </Fragment>
