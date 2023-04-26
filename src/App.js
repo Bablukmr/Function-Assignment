aa11ae686315c9a5e588dc2c095bd9a79745982c
Bablukmr <bablukmr800@gmail.com>
2023-04-26 22:49:45 +0530

    solution of useState2 Assignment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-<from>
-  <label>
-      <input placeholder='Add Your Name...'/>
-      <button>Submit</button>
-      </label>
-      <h2>Hi My Name Is : </h2>
-      </from>
-    </div>
+  const [name, setName] = useState("");
+  const [Text, setText] = useState("");
 
+  const Submit = (event) => {
+    event.preventDefault();
+    setText(name);
+    setName("");
+  };
+
+  const nameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <div className='App'>
+      <form onSubmit={Submit}>
+        <label>
+          <input placeholder='Add Your Name...' type="text" value={name} onChange={nameChange} />
+          <button type="submit">Submit</button>
+        </label>
+        <h1>Hi My Name is : {Text}</h1>
+
+      </form>
+    </div>
   );
 }
 
