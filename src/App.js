d725c1dd37fc1ae93f0a285cb22ef5d3cce7b0f7
Bablukmr <bablukmr800@gmail.com>
2023-04-26 17:34:26 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -4,21 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<from>
+  <label>
+      <input placeholder='Add Your Name...'/>
+      <button>Submit</button>
+      </label>
+      <h2>Hi My Name Is : </h2>
+      </from>
     </div>
+
   );
 }
 
