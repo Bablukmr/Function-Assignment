342ebf8fe025a905a4d9d9a0ee48b61736f987c8
Bablukmr <bablukmr800@gmail.com>
2023-05-01 21:46:30 +0530

    solution of file uploader

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function click(){
   alert(name)
 }
 
-
   return (
    <div className='App'>
       <File />
--- src/fileUploader.js
+++ src/fileUploader.js
@@ -3,15 +3,16 @@ import { useRef } from "react";
 function File() {
   const fileRef = useRef(null);
 
-  function FileUploader() {
+  function fileUploader() {
     fileRef.current.click();
   }
 
+  
   return (
     <div>
       <h1 style={{ color: "blue" }}>Upload Your File </h1>
       <input type="file" ref={fileRef} />
-      <button  onClick={FileUploader}>
+      <button  onClick={fileUploader}>
         picke the file
       </button>
     </div>
