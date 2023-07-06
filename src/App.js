0697ca0fcb3c5317a6188da5d99ae6c5b18c4926
BABLU KUMAR <bablukmr800@gmail.com>
2023-07-06 18:44:42 +0530

    Assignment 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
-import Calculator from './components/calculator/Calculator';
+import React, { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState("");
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const generateTable = (number) => {
+    const table = [];
+    for (let i = 1; i <= number; i++) {
+      table.push(
+        <tr  key={i}>
+          <td style={{border:'1px solid black'}}>{i}</td>
+        </tr>
+      );
+    }
+    return table;
+  };
+
   return (
-    <div className="App">
-<Calculator/>
+    <div>
+      <input
+        type="number"
+        value={number}
+        onChange={handleNumberChange}
+        placeholder="Enter a number"
+      />
+      <table>
+        <tbody>{generateTable(Number(number))}</tbody>
+      </table>
     </div>
   );
 }
